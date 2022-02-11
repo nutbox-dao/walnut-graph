c1904bddf1c197ef35e0851ac966696a96774be6
wangxi <wangxi8206@sina.com>
2022-02-11 10:18:10 +0800

    update

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,5 +1,5 @@
 import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 
-export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x0bAA558e21CCb7b394DcBf0832A18D7c5927804F"));
-export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x811DDDB1C40de20741fb3d3FF320B47A2d13Ebc3'));
-export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x7Ca26632B4D9Ba763D78f657Ef86AEa1A5a221b3'));
+export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x061396e85554D473F5497BB65E05B093b131c4d6"));
+export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x6e77E0f5750C29951281D77d12239FfF439a7eae'));
+export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0xC27b4Ab154318431cD926F3aE6cB846Af7294aB8'));
