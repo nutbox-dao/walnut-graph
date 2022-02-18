5222ae8e98d0e1c60fc4aa492ff83eb09f4ad4ec
wangxi <wangxi8206@sina.com>
2022-02-18 14:41:51 +0800

    add cosmos

--- src/contracts.ts
+++ src/contracts.ts
@@ -3,3 +3,5 @@ import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x5245dd378Ed1B0aE4aE41fFa28019a910bC0648D"));
 export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x49589033a856A6B9cFE21832e5A0e4cB1EC411Ea'));
 export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x212074e18CE6b81064C8AbBBbaEb3e6dF7a4f27d'));
+export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0x49589033a856A6B9cFE21832e5A0e4cB1EC411Ea'));
+
