6f95e2776ccb36942eab0ebe249e679e72027e9d
wangxi <wangxi8206@sina.com>
2022-02-24 12:36:10 +0800

    init

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,5 +1,5 @@
 import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 
-export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0xd10e4C1e301A13A9B874bd1757c135Eda075769D"));
+export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0xF7Fa41BF814eDC767691DDB1864a334D83f4acf7"));
 export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0xF7Fa41BF814eDC767691DDB1864a334D83f4acf7'));
-export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0xf870724476912057C807056b29c1161f5Fe0199a'));
+export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x00ad4C61403455731C685cC71D9a406c5fcB8D69'));
