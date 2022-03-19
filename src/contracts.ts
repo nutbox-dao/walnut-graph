18811de64fcf8441db10774078daa07280b52c87
wangxi <wangxi8206@sina.com>
2022-03-19 20:24:31 +0800

    update contract

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,7 +1,7 @@
 import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 
-export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x971e67D40DE7D9d432c37BD33Fc8F17891f849FF"));
-export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0xa91c943fDa447D650F5F22BeF846F6916D5F84d6'));
-export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x8F5bc032CED83F87214093ca3871369D2004D97f'));
-export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0xBF2Fd8Ca75D4784950A4C8Ee450fC8705713E6E7'));
+export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x855d44dd7Ac700E8bdfB06182Cb51d7465229D80"));
+export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x3dF2B8FA0bc4270ac62AF010e70bB9234aD67dac'));
+export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x8b6C7e5B79927223b46550725f0195fc778231BE'));
+export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0xc54EeC4807D980d3E566d06e91c1152573956947'));
 
