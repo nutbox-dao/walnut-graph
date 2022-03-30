1bb5fa5ee2c40695504a0afaa21eed98d1e480d7
wangxi <wangxi8206@sina.com>
2022-03-30 10:29:12 +0800

    update cosmos chain contract

--- src/contracts.ts
+++ src/contracts.ts
@@ -3,5 +3,5 @@ import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0xd63fF0c26f14Aa9f1Dc05549736Dc19d2Ec077C8"));
 export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x2b924ED31eD8680bEF8B89D9cd9104DAF2eC4f01'));
 export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x38e847AF9b546336caf3ba10f65C125C95B1d93F'));
-export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0x2B8b2c28CD4b05288e6EcC41312D42b59Af3e24a'));
+export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0x74790fF941A91881Af152a3e6649C34EDC6646cd'));
 
