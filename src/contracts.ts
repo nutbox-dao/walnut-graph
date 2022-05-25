12e6f019e386b1c48b5bcd0741a3f074d79806a2
wangxi <wangxi8206@sina.com>
2022-05-25 14:20:17 +0800

    update contract

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,6 +1,6 @@
 import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 
-export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x1c5765912e5288d956d8cF004a8002ACD1bbA649"));
-export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x8ad7cb0b962d0C8d6044E1Ed7Ea491f6b0f5F965'));
-export const CrowdloanFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x52E78f8e68f4a5ab0aeB057F0a9be40A18dA7d3d'));
-export const AstarDappStakingFactory: Bytes=   Bytes.fromByteArray(ByteArray.fromHexString("0xF3f8a37E517A3c2378Cf99A3C43e06f011393B2b"))
+export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x3931546cA5399ef0e0b0c7d1d78C89F0e7BeB8AF"));
+export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0xF46ddB1Cb72e648138bc902d5D0E984444dC83c5'));
+export const CrowdloanFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x5F75409D6BB35D382eF11aaD614EE107ec6FE77f'));
+export const AstarDappStakingFactory: Bytes=   Bytes.fromByteArray(ByteArray.fromHexString("0xff112503B6C860a578b9BA868d19F8D3859DCcBA"))
