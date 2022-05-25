0ca988271c68517979fc3df1d1cbefc94716b807
wangxi <wangxi8206@sina.com>
2022-05-25 14:33:07 +0800

    udpate contract

--- src/contracts.ts
+++ src/contracts.ts
@@ -3,4 +3,4 @@ import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0x3931546cA5399ef0e0b0c7d1d78C89F0e7BeB8AF"));
 export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0xF46ddB1Cb72e648138bc902d5D0E984444dC83c5'));
 export const CrowdloanFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x5F75409D6BB35D382eF11aaD614EE107ec6FE77f'));
-export const AstarDappStakingFactory: Bytes=   Bytes.fromByteArray(ByteArray.fromHexString("0xff112503B6C860a578b9BA868d19F8D3859DCcBA"))
+export const AstarDappStakingFactory: Bytes=   Bytes.fromByteArray(ByteArray.fromHexString("0xbBAc3A06eB1918EfC33af78e83E9A3b2D9fe81f3"))
