cc1735114772704ffd8fea966485330ed5006d40
wangxi <wangxi8206@sina.com>
2023-06-12 16:27:40 +0800

    update to arbitrum

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ## Walnut graph
+
+The graph code for walnut of nutbox
+
+### Config
+Deploy nutbox contract first [nutbox]https://github.com/nutbox-dao/contract
+Update the subgraph.yaml and /src/contracts.ts file with your depolyed contract
+
+### Init
+```bash
+yarn
+graph codegen && graph build
+```
+
+
+### Deploy
+```bash
+graph auth --product hosted-service [your-key]
+graph deploy --product hosted-service [your-page]
+```
--- src/contracts.ts
+++ src/contracts.ts
@@ -1,7 +1,7 @@
 import { Bytes, ByteArray } from '@graphprotocol/graph-ts';
 
-export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0xd10e4C1e301A13A9B874bd1757c135Eda075769D"));
-export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0xF7Fa41BF814eDC767691DDB1864a334D83f4acf7'));
-export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0xf870724476912057C807056b29c1161f5Fe0199a'));
-export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0xAD6a0c0017559d051264e1657d627107d6b12f0d'));
-export const ERC1155StakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0x76303Be21ef601e68639B541B035ca33d247b5FE'));
+export const Committee: Bytes            =     Bytes.fromByteArray(ByteArray.fromHexString("0xA643e598364A9dFB3328aD2E70AF6f9E3C477A42"));
+export const SPStakingFactory: Bytes     =     Bytes.fromByteArray(ByteArray.fromHexString('0x183434ba0726b244521cB1C46AE5C90538146db8'));
+export const ERC20StakingFactory: Bytes  =     Bytes.fromByteArray(ByteArray.fromHexString('0x420E3b63F2587702B0BCdc50aF948cF387515593'));
+export const CosmosStakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0xFe992EF5f73Ac289052F1742B918278a62686fD1'));
+export const ERC1155StakingFactory: Bytes =     Bytes.fromByteArray(ByteArray.fromHexString('0x20ABc409b7dc7a6DC8cC1309A5A7DBb5B1c0D014'));
